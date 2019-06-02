52f628518cbedac49f95210b910edf8dbca89271
Ilfat <ilfat.sabirzyanov@raxeltelematics.com>
2019-06-02 14:13:06 +0300

    added MessageLibrary

---
+++ MessageLibrary/ButtonCombination.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9Library
+{
+    public class ButtonCombination
+    {
+        private string combination;
+        public ButtonCombination(char[] buttons)
+        {
+            Buttons = buttons;
+            combination = new string(buttons);
+        }
+
+        public char[] Buttons { get; }
+
+        public override string ToString()
+        {
+            return combination;
+        }
+
+        public static ButtonCombination CreateFromDigits(params int[] digits)
+        {
+            int offset = (int)'0';
+            char[] chars = new char[digits.Length];
+            for (int i = 0; i < digits.Length; i++)
+            {
+                chars[i] = (char)(digits[i] + offset);
+            }
+            return new ButtonCombination(chars);
+        }
+    }
+}
---
+++ MessageLibrary/MessageConverterException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9Library
+{
+    public class MessageConverterException : Exception
+    {
+        public MessageConverterException(string message) : base(message)
+        {
+        }
+    }
+}
