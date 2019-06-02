3b8483687cc4f1c325ba828944e8c5cfa6fdcac5
Ilfat <ilfat.sabirzyanov@raxeltelematics.com>
2019-06-02 10:57:29 +0300

    Add project files.

---
+++ T9Library/T9Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9Library
+{
+    public class T9Converter
+    {
+        public string Translate(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                return string.Empty;
+            }
+
+            return "44 444";
+        }
+    }
+}
---
+++ T9LibraryTests/T9ConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using T9Library;
+using Xunit;
+
+namespace T9LibraryTests
+{
+    public class T9ConverterTests
+    {
+        [Fact]
+        public void Translate_WhenInputEmpty_ReturnsEmpty()
+        {
+            var converter = new T9Converter();
+            var result = converter.Translate(string.Empty);
+            Assert.Equal(string.Empty, result);
+        }
+
+        [Theory]
+        [InlineData("hi", "44 444")]
+        public void Translate(string message, string translatedMessage)
+        {
+            var converter = new T9Converter();
+            var result = converter.Translate(message);
+            Assert.Equal(translatedMessage, result);
+        }
+    }
+}
